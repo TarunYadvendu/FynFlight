aaa04520f7539aff07a79a1b96819436dc58405c
Yuvraj <yuvraj@fynancial.com>
2026-09-23 13:10:26 +0530

    api link fix

--- src/services/apiInstance.ts
+++ src/services/apiInstance.ts
@@ -17,7 +17,7 @@ interface RequestOptions {
 }
 
 export const apiClient = axios.create({
-  baseURL: 'http://192.168.1.221:3001/',
+  baseURL: 'http://18.219.138.66:3001/',
   timeout: 15000,
   headers: {
     Accept: 'application/json',
